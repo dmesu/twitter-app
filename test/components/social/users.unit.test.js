fe5cfde591f98f486809aa0c75425c729fa95735
Daida Medina <daida.ms@gmail.com>
2021-06-11 17:07:14 +0100

    Fix unit test.

--- test/components/social/users.unit.test.js
+++ test/components/social/users.unit.test.js
@@ -2,15 +2,11 @@
 const test = require('ava');
 const sinon = require('sinon');
 const request = require('supertest');
+const SocialService = require('../../../src/components/social/social.service');
 
 require('dotenv').config();
 
-const { SocialService } = require('../../../src/components/social/social.service');
-const getUsers = sinon.stub(SocialService, 'getUsers').resolves([{
-  _id: "60afb662ad6cbe1d1ae85850",
-  username: "peter"
-}]);
-
+const getUsers = sinon.stub(SocialService, 'getUsers')
 const app = require('../../../src/app');
 
 test.before(async (t) => {
@@ -20,14 +16,23 @@ test.before(async (t) => {
 });
 
 test.after.always((t) => {
+  getUsers.restore();
   delete require.cache[require.resolve('../../../src/app')]; // kills server
 });
 
 test('Fetch All Users', async (t) => {
   const { server, apiUrl, stubs } = t.context;
+  
+  const users = [{
+    _id: "60afb662ad6cbe1d1ae85850",
+    username: "peter"
+  }]
+
+  stubs.getUsers.returns(Promise.resolve(users));
+
   const res = await server
     .get(apiUrl)
     .expect(200);
   t.true(stubs.getUsers.calledOnce);
-  t.true(res.body.data.length > 0);
+  t.true(res.body.data.length === 1);
 });
